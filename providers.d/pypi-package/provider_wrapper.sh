a42c394f8541182c5f9c0b3d6433eda0ac7c1e75
pcseanmckay <pc.seanmckay@gmail.com>
2019-03-08 13:48:39 -0600

    Update pypi package provider provider wrapper script.
    Updated script to exit on failures to created necessary directories.

--- providers.d/pypi-package/provider_wrapper.sh
+++ providers.d/pypi-package/provider_wrapper.sh
@@ -1,75 +1,20 @@
-bashelliteProviderWrapperBandersnatch() {
-
-  # Perform some pre-sync checks
-  read _ pypi_status_code _ < <(curl -sI "${_n_repo_url}");
-
-  if [[ "${pypi_status_code:0:1}" < "4" ]]; then
-    utilMsg INFO "$(utilTime)" "The pypi mirror appears to be up; sync should work...";
-  else
-    utilMsg FAIL "$(utilTime)" "The pypi mirror appears to be down/invalid/inaccessible; exiting...";
-    return 1;
-  fi
-
-  local directory_parameter="$(grep -oP "(?<=(^directory = )).*" ${_r_metadata_tld}/repos.conf.d/${_n_repo_name}/provider.conf)"
-  if [[  "${directory_parameter}" != "${_r_mirror_tld}/${_n_repo_name}" ]]; then
-    utilMsg WARN "$(utilTime)" "The \"directory\" parameter (${directory_parameter}) in provider.conf does not match mirror location (${_r_mirror_tld}/${_n_repo_name})..."
-  fi
-
-  if [ ! -d ${HOME}/.bashellite ]; then
-    utilMsg WARN "$(utilTime)" "The temporary directory (${HOME}/.bashellite) doesn't exist.  Creating..."
-    mkdir -p ${HOME}/.bashellite
-  fi
+bashelliteProviderWrapperPypi() {
 
   local config_file="${_r_metadata_tld}/repos.conf.d/${_n_repo_name}/provider.conf"
-  local tmp_config_file="${HOME}/.bashellite/tmp_${_n_repo_name}_provider.conf"
-
-  > ${tmp_config_file}
-
-  local line_counter=0
-  while IFS=$'\n' read line; do
-    if [[ ${line} =~ ^directory[[:blank:]]*[=][[:blank:]]*[[:alnum:]/_.-]+ ]]; then
-      if [ "${line_counter}" = 0 ]; then
-        echo "directory = ${_r_mirror_tld}/${_n_repo_name}" >> ${tmp_config_file}
-      else
-        utilMsg WARN "$(utilTime)" "Duplicate directory parameter found.  Skipping line..."
-      fi
-      line_counter=$(($line_counter + 1))
-    else
-      echo "${line}" >> ${tmp_config_file}
-    fi
-  done < ${config_file}
-
-  utilMsg INFO "$(utilTime)" "Proceeding with sync of repo (${_n_repo_name}) using ${_n_repo_provider}..."
-  # If dryrun is true, perform dryrun
-  if [[ ${_r_dryrun} ]]; then
-    utilMsg INFO "$(utilTime)" "Sync of repo (${_n_repo_name}) using ${_n_repo_provider} completed without error..."
-  # If dryrun is not true, perform real run
-  else
-    ${_r_providers_tld}/bandersnatch/exec/bin/bandersnatch -c "${tmp_config_file}" mirror;
-    if [[ "${?}" == "0" ]]; then
-      utilMsg INFO "$(utilTime)" "Sync of repo (${_n_repo_name}) using ${_n_repo_provider} completed without error...";
-    else
-      utilMsg WARN "$(utilTime)" "Sync of repo (${_n_repo_name}) using ${_n_repo_provider} did NOT complete without error...";
-    fi
-  fi
-
-
-####################################################################
-
   local pypi_url="${_n_repo_url}"
   local base_dir="${_r_mirror_tld}/${_n_repo_name}/web"
   mkdir -p "${base_dir}" &>/dev/null \
-      || Fail "Unable to create directory (${base_dir}); check permissions."
+      || { utilMsg FAIL "$(utilTime)" "Unable to create directory (${base_dir}); check permissions."; return 1; }
 
   local simple_dir="${base_dir}/simple"
 
   mkdir -p "${simple_dir}" &>/dev/null \
-      || Fail "Unable to create directory (${simple_dir}); check permissions."
+      || { utilMsg FAIL "$(utilTime)" "Unable to create directory (${simple_dir}); check permissions."; return 1; }
 
   local packages_dir="${base_dir}/packages"
 
   mkdir -p "${packages_dir}" &>/dev/null \
-      || Fail "Unable to create directory (${packages_dir}); check permissions."
+      || { utilMsg FAIL "$(utilTime)" "Unable to create directory (${packages_dir}); check permissions."; return 1; }
 
   while read package_line; do 
     # Step 1. Get package listing from $pypi_url/simple/$mirror_package_name/ 
@@ -78,13 +23,14 @@ bashelliteProviderWrapperBandersnatch() {
     # Step 3. Save non package listing lines to previous index.html
     # Step 4. For each package listing, download package and save
     # it to $base_dir/packages/<packages directory>
+    utilMsg INFO "$(utilTime)" "Processing package: ${package_line}"
 
     mirror_package_name=${package_line}
 
     local package_name_dir="${simple_dir}/${mirror_package_name}"
 
     mkdir -p "${package_name_dir}" &>/dev/null \
-        || Fail "Unable to create directory (${package_name_dir}); check permissions."
+        || { utilMsg FAIL "$(utilTime)" "Unable to create directory (${package_name_dir}); check permissions."; return 1; }
 
     cat /dev/null > ${package_name_dir}/index.html
 
@@ -122,7 +68,7 @@ bashelliteProviderWrapperBandersnatch() {
         package_url_dir_path=${package_url_dir_path%/*}
 
         mkdir -p "${packages_dir}/${package_url_dir_path}" &>/dev/null \
-          || Fail "Unable to create directory (${packages_dir}/${package_url_dir_path}); check permissions."
+          || { utilMsg FAIL "$(utilTime)" "Unable to create directory (${packages_dir}/${package_url_dir_path}); check permissions."; return 1; }
 
         # Now check to see if file from package listing already exists
         if [[ ${package_file_name} != "" ]]; then
@@ -140,10 +86,10 @@ bashelliteProviderWrapperBandersnatch() {
 
           # Save file if file_save == "true"
           if [[ ${file_save}  == "true" ]]; then
-            echo "Saving File: ${package_file_name}..."
+            utilMsg INFO "$(utilTime)" "Saving File: ${package_file_name}..."
             curl -s -S -o "${packages_dir}/$package_url_dir_path/${package_file_name}" ${package_url}
           else
-            echo "File: ${package_file_name} already exists, skipping..."
+            utilMsg INFO "$(utilTime)" "File: ${package_file_name} already exists, skipping..."
           fi
         fi
       fi
